c39344971934c0e887aadf98b57654e9b440c693
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-05-20 11:37:15 +0200

    refactor

--- src/main/scala/RailFenceCipher.scala
+++ src/main/scala/RailFenceCipher.scala
@@ -3,13 +3,16 @@ object RailFenceCipher:
       case E, D
    def encode(s: String, i: Int): String = rfc(s, i, Mode.E)
    def decode(s: String, i: Int): String = rfc(s, i, Mode.D)
-   private def p(k: Int): Seq[Int] = (0 until k) ++ (k - 2 until 0 by -1)
+   private def pat(k: Int): Seq[Int] = (0 until k) ++ (k - 2 until 0 by -1)
    private def rfc(s: String, i: Int, m: Mode): String =
+      lazy val (p, pSize, sLen) = (pat(i), pat(i).size, s.length)
+      lazy val rep = (0 until sLen).flatMap(_ => p).sliding(sLen, sLen).next()
       m match
-         case Mode.E =>
-            s.sliding(p(i).size, p(i).size).flatMap(_.zip(p(i))).toSeq.groupBy(_._2).values.flatten.map(_._1).mkString
+         case Mode.E => s.sliding(pSize, pSize).flatMap(_.zip(p)).toSeq.groupBy(_._2).values.flatten.map(_._1).mkString
          case Mode.D =>
             (0 until i)
-               .flatMap(r =>
-                  (0 until s.length).zip(LazyList.continually(p(i)).flatten.take(s.length)).filter(_._2 == r).map(_._1))
-               .zip(s).sortBy(_._1).map(_._2).mkString
+               .flatMap(j => (0 until sLen).zip(rep).filter(_._2 == j).map(_._1))
+               .zip(s)
+               .sorted
+               .map(_._2)
+               .mkString
